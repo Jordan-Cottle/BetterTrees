5ab29abd3706c7e8aca3d53a6e54f659b330b0b5
JordanCottle <jordancottle622@gmail.com>
2020-01-12 17:57:01 -0500

    Added tree breaking functionality

--- src/main/java/cottle/jordan/bettertrees/TreeListener.java
+++ src/main/java/cottle/jordan/bettertrees/TreeListener.java
@@ -1,6 +1,12 @@
 package cottle.jordan.bettertrees;
 
+import java.util.EnumSet;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
 import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -9,24 +15,73 @@ import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockDamageEvent;
 
 public class TreeListener implements Listener {
+    private Map<Player, TreeTracker> trees;
+
+    private static final Set<Material> woodTypes;
+
+    static{
+        woodTypes = EnumSet.of(
+            Material.ACACIA_LOG,
+            Material.BIRCH_LOG,
+            Material.DARK_OAK_LOG,
+            Material.JUNGLE_LOG,
+            Material.OAK_LOG,
+            Material.SPRUCE_LOG,
+
+            Material.STRIPPED_ACACIA_LOG,
+            Material.STRIPPED_BIRCH_LOG,
+            Material.STRIPPED_DARK_OAK_LOG,
+            Material.STRIPPED_JUNGLE_LOG,
+            Material.STRIPPED_OAK_LOG,
+            Material.STRIPPED_SPRUCE_LOG
+            );
+    }
+
+    public TreeListener(){
+        trees = new HashMap<Player, TreeTracker>();
+    }
+
     @EventHandler
     public void onBreak(BlockBreakEvent event){
-        Block block = event.getBlock();
-        
         Player player = event.getPlayer();
 
-        Location pos = block.getLocation();
+        if(!trees.containsKey(player)){
+            return;
+        }
+        TreeTracker tree = trees.get(player);
+        Block block = event.getBlock();
 
-        player.sendMessage("You broke a block at " + pos.toString() + "!");
+        if(!tree.rootEquals(block)){
+            return;
+        }
+        
+        Location pos = block.getLocation();
+        player.sendMessage("You broke a tree rooted at " + pos.toString() + "!");
+
+        tree.transform(Material.AIR);
     }
 
     @EventHandler
     public void onDamage(BlockDamageEvent event){
         Block block = event.getBlock();
+        if(!woodTypes.contains(block.getType())){
+            return;
+        }
+        
         Player player = event.getPlayer();
+        TreeTracker tree;
+        if(!trees.containsKey(player)){
+            player.sendMessage("Creating a new tree for you!");
+            tree = new TreeTracker(block);
 
-        player.sendMessage("Oof that hurts!");
-        System.out.println(block.getType().toString() + " was murdered!");
-        event.setInstaBreak(true);
+            trees.put(player, tree);
+        }else{
+            tree = trees.get(player);
+
+            if(!tree.rootEquals(block)){
+                tree.clear();
+                trees.replace(player, new TreeTracker(block));
+            }
+        }
     }
 }
